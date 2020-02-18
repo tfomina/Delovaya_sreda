df415726dabfa8e031a75833037255cda471f0fe
Tatyana Fomina <tayna-anita@mail.ru>
2020-02-18 17:27:42 +0300

    Добавила вывод лицензии в компоненте item, изменила дефолтные пропсы

--- src/components/List/Item.js
+++ src/components/List/Item.js
@@ -2,13 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Stars } from "./Stars";
 
-export const Item = ({ item = {} }) => (
+export const Item = ({ item }) => (
   <li className="list-group-item d-flex flex-column flex-lg-row justify-content-between align-items-start align-items-lg-center flex-nowrap">
     <div>
-      <b>{item.name}</b>{" "}
-      <a href={item.html_url} target="_blank" rel="noopener noreferrer">
-        {item.full_name}
-      </a>
+      <div>
+        <b>{item.name}</b>{" "}
+        <a href={item.html_url} target="_blank" rel="noopener noreferrer">
+          {item.full_name}
+        </a>
+      </div>
+      <div>Лицензия: {(item.license && item.license.name) || "Не указано"}</div>
     </div>
     <Stars count={item.stargazers_count} />
   </li>

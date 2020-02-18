867b3febe338fa8a22161b6155fd1a2aebf15fcc
Tatyana Fomina <tayna-anita@mail.ru>
2020-02-18 19:24:32 +0300

    Переделала компонент App с использование useReducer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useReducer } from "react";
 import axios from "axios";
 import moment from "moment";
 import { useDebounce } from "use-debounce";
@@ -14,20 +14,84 @@ import "./App.css";
 
 const PER_PAGE = 20;
 
-export const App = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasError, setHasError] = useState(false);
-  const [nameSearch, setNameSearch] = useState("");
-  const [license, setLicense] = useState({});
-  const [currentPage, setCurrentPage] = useState(1);
-  const [total, setTotal] = useState(0);
+const initialState = {
+  data: [],
+  isLoading: false,
+  hasError: false,
+  nameSearch: "",
+  license: {},
+  currentPage: 1,
+  total: 0
+};
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_DATA_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        hasError: false,
+        data: [],
+        total: 0
+      };
+    case "FETCH_DATA_SUCCESS":
+      const { data, total } = action.payload;
+      return {
+        ...state,
+        isLoading: false,
+        hasError: false,
+        data,
+        total
+      };
+    case "FETCH_DATA_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        hasError: true,
+        data: [],
+        total: 0
+      };
+    case "SET_CURRENT_PAGE":
+      const { currentPage } = action.payload;
+      return {
+        ...state,
+        currentPage
+      };
+    case "SET_NAME_SEARCH":
+      const { nameSearch } = action.payload;
+      return {
+        ...state,
+        nameSearch,
+        currentPage: 1
+      };
+    case "SET_lICENSE":
+      const { license } = action.payload;
+      return {
+        ...state,
+        license,
+        currentPage: 1
+      };
+    default:
+      throw new Error();
+  }
+};
+
+export const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const {
+    data,
+    isLoading,
+    hasError,
+    nameSearch,
+    license,
+    currentPage,
+    total
+  } = state;
   const [debouncedNameSearch] = useDebounce(nameSearch, 500);
 
   const fetchData = useCallback(async () => {
-    setHasError(false);
-    setIsLoading(true);
+    dispatch({ type: "FETCH_DATA_PENDING" });
 
     try {
       const prevMonth = moment()
@@ -41,27 +105,59 @@ export const App = () => {
       }&sort=stars&order=desc&page=${currentPage}&per_page=${PER_PAGE}`;
 
       const response = await axios(url);
-      setData(response.data.items);
-      setTotal(response.data.total_count);
+      dispatch({
+        type: "FETCH_DATA_SUCCESS",
+        payload: {
+          data: response.data.items,
+          total: response.data.total_count
+        }
+      });
     } catch (error) {
-      setHasError(true);
-      setData([]);
+      dispatch({
+        type: "FETCH_DATA_ERROR"
+      });
     }
-    setIsLoading(false);
   }, [license, debouncedNameSearch, currentPage]);
 
   useEffect(() => {
     fetchData();
   }, [license, debouncedNameSearch, currentPage, fetchData]);
 
+  const handleLicenseChange = value => {
+    dispatch({
+      type: "SET_lICENSE",
+      payload: {
+        license: value
+      }
+    });
+  };
+
+  const handleNameSearchChange = value => {
+    dispatch({
+      type: "SET_NAME_SEARCH",
+      payload: {
+        nameSearch: value
+      }
+    });
+  };
+
+  const handlePageChange = value => {
+    dispatch({
+      type: "SET_CURRENT_PAGE",
+      payload: {
+        currentPage: value
+      }
+    });
+  };
+
   return (
     <Layout>
       <Header>
         <Search
-          handleNameSearchChange={setNameSearch}
+          handleNameSearchChange={handleNameSearchChange}
           nameSearch={nameSearch}
         />
-        <Licenses license={license} handleLicenseChange={setLicense} />
+        <Licenses license={license} handleLicenseChange={handleLicenseChange} />
       </Header>
 
       <main>
@@ -76,7 +172,7 @@ export const App = () => {
               currentPage={currentPage}
               total={total}
               itemsPerPage={PER_PAGE}
-              handlePageChange={setCurrentPage}
+              handlePageChange={handlePageChange}
             />
           </>
         )}
